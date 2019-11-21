a9222cc54e5fde5d64b9356f277dbfcbb22374d8
Leonardo Soares <leferso@gmail.com>
2019-11-21 05:56:08 -0300

    Adicionando verificacao de rotas autenticadas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Router } from "react-router-dom";
+
+import history from "./services/history";
+import "./config/ReactotronConfig";
+
+import Routes from "./routes";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
 
 export default App;
